a3047ad180f0e01c54092df3bce9578e69ef47a5
ArinaLuzgina <LusginaArinaA@gmail.com>
2025-02-21 03:04:46 +0300

    2lab. Kind of ready

---
+++ lab2/build/CMakeFiles/some_physics.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/some_physics.dir/problems/some_physics.cpp.o"
+  "CMakeFiles/some_physics.dir/problems/some_physics.cpp.o.d"
+  "some_physics"
+  "some_physics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/some_physics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
